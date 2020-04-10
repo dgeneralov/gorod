f618dab3b2b6a0b3ac9ffd13a61cb5576502f0e4
dgeneralov <60974858+dgeneralov@users.noreply.github.com>
2020-04-10 09:31:47 +0300

    css

---
+++ js/fp-function.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function($) {
+    $("#fullpage").fullpage({
+   sectionsColor:['tranporent'],
+   anchors:['home','history','diskography','photo','video','contacts'],
+   menu:'#menu',
+   scrollBar:false,
+   scrollOverflow:true,
+   navigation:true,
+   navigationTooltips:['Секция 1','Секция 2','Секция 3'],
+   slidesNavigation:true,
+   navigationPosition:'left',
+   slidesNavPosition:'top',
+   loopTop:true,
+   loopBottom:true,
+   loopHorizontal:false,
+   
+   afterResize:function(link,index) {
+   alert('Hello');
+   },
+   afterLoad:function(link,index) {
+   
+   if(index == 3) {
+   $("#section2 img").delay(2000).animate({'left':'0%'},2000);
+   }
+   if(link == '3rdPage') {
+   $("#section2 h1").fadeIn(1500,function() {
+   $("#section2 p").css({'display':'block'}).animate({'fontSize':'8em'},1000)
+   });
+   }
+   
+   }
+   
+    });
+   })
